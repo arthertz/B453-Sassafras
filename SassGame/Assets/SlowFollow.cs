39a7b95b88fdb57440f4b6a60ea44427328531a8
arthertz <arthertz@iu.edu>
2021-03-18 01:41:17 -0400

    Made a better demo scene
    It should be easier to explore the simple generated world now.
    I also slightly tweaked the generation to make smoother/more coherent caves.

---
+++ SassGame/Assets/SlowFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float thresh;
+
+    [SerializeField] float springForce;
+
+    new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) < thresh) {
+            rigidbody.velocity = Vector3.zero;
+        } else {
+            rigidbody.AddForce((target.position - transform.position) * springForce, ForceMode.Acceleration);
+        }
+    }
+}
